cb8e272890e63c579d551b7bc74596bb88248134
justbeingashutosh <itsashutosh.dev@gmail.com>
2025-02-24 00:36:02 +0530

    Added copy btn

--- public/script.js
+++ public/script.js
@@ -1,13 +1,25 @@
 const submit = document.getElementById('submit')
 const inputfield = document.getElementById('longurl')
 const resdiv = document.getElementById('result')
+const resdivContainer = document.getElementById('bottom')
+const copybtn = document.getElementById("copy")
 inputfield.focus()
 submit.addEventListener('click', async()=>{
     const longurl = document.getElementById('longurl').value
     const msg = await axios.post('/api/shorten', {longurl: longurl})
-    resdiv.style.display = 'flex';
     resdiv.textContent = msg.data.msg
+    if(msg.data.msg === "Please enter a valid URL!"){
+    resdivContainer.style.display = 'flex';
+    copybtn.style.display = 'none'
+    }else{
+    resdivContainer.style.display = 'flex';
+    copybtn.style.display = 'block'
+    }
 })
 inputfield.addEventListener('focus', ()=>{
-    resdiv.style.display = 'none'
+    resdivContainer.style.display = 'none'
+})
+
+copybtn.addEventListener('click', ()=>{
+    navigator.clipboard.writeText(resdiv.textContent)
 })
